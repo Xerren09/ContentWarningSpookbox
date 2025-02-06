c0e384a78384f19909db683e3cf54992d2a28b61
Bars Margetsch <45538919+Xerren09@users.noreply.github.com>
2025-02-06 20:41:30 +0200

    Add dedicated PluginLoader and dependency error popup
    This should resolve the rare case where ShopAPI is loaded second when the mods are installed while the game is running. The loader will detect this and show a prompt to the player to restart their game.

--- SpookboxPlugin.cs
+++ SpookboxPlugin.cs
@@ -3,11 +3,9 @@ using UnityEngine;
 using ContentWarningShop;
 using ContentWarningShop.Localisation;
 using Spookbox.Behaviour;
-using System.Collections;
-#if MODMAN
-using BepInEx;
-using HarmonyLib;
-#endif
+using Spookbox.Settings;
+using Steamworks;
+using Zorro.Settings;
 
 /*
     TASKS:
@@ -20,56 +18,50 @@ using HarmonyLib;
     TODO: Scroll to swap tracks
     TODO: Right click hold on shoulder - plays louder?
     TODO: Localise strings
- */
+*/
 
 namespace Spookbox
 {
     // This partial class file is used purely to separate the mess that is the multi target setup.
     // Define mod setup changes between the vanilla mod loader and bepinx
-    [ContentWarningPlugin(MOD_GUID, MOD_VER, false)]
-#if MODMAN
-    [BepInPlugin(MOD_GUID, MOD_NAME, MOD_VER)]
-    [BepInDependency(ShopApiPlugin.MOD_GUID)]
-#endif
     public partial class SpookboxPlugin
-#if MODMAN
-    : BaseUnityPlugin
-#endif
     {
-        public static string PluginDirPath => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         public const string MOD_GUID = "xerren.cwspookbox";
         public const string MOD_NAME = "Spöökbox";
-        public const string MOD_VER = "1.0.0";
+        public const string MOD_VER = "1.1.0";
+        private const string ASSETBUNDLE_NAME = "spookbox";
+        internal static string PluginDirPath => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-        internal static Item _spookboxItem;
-        internal const string SPOOKBOX_GUID = "91f31218-6507-4bef-928d-e76b33f44a51";
-        internal static AssetBundle _bundle;
-#if MODMAN
-        private static Harmony harmony = new Harmony(MOD_GUID);
-#endif
+        private static bool _initialised = false;
+        private static bool _initialisedSettings = false;
 
+        /// <summary>
+        /// Asset bundle containing all the mod's assets.
+        /// </summary>
+        internal static AssetBundle? _bundle;
+        /// <summary>
+        /// Base <see cref="ScriptableObject"/> <see cref="Item"/>. All spawned items are configured from this.
+        /// </summary>
+        internal static Item? _spookboxItem;
+        /// <summary>
+        /// The Item GUID used by the Spookbox item. Not directly used, but encoded in the dll for convenience.
+        /// </summary>
+        public const string SPOOKBOX_GUID = "91f31218-6507-4bef-928d-e76b33f44a51";
 
-#if STEAM
-        static SpookboxPlugin() 
-#elif MODMAN
-        void Awake()
-#endif
+        private static Callback<LobbyCreated_t> cb_onLobbyCreated;
+
+        private const string ALERT_META_GUID = "bigslapTunes";
+        internal static readonly SynchronisedMetadata<bool> AlertMonsters = new($"{MOD_GUID}_{ALERT_META_GUID}", true);
+
+        private const string INF_BATTERY_META_GUID = "infiniteBattery";
+        internal static readonly SynchronisedMetadata<bool> InfiniteBattery = new($"{MOD_GUID}_{INF_BATTERY_META_GUID}", false);
+
+        internal static void InitialisePlugin()
         {
-            Initialise();
-#if STEAM
-            Debug.Log($"{MOD_GUID} initialising via the vanilla mod loader.");
-            GameHandler.Instance.StartCoroutine(Deferred_LoadSettings());
-#elif MODMAN
-            Debug.Log($"{MOD_GUID} initialising via BepInEx mod loader.");
-            // Mod will initialise on GameHandler.Initialize
-            harmony.PatchAll();
-#endif
-            Debug.Log($"{MOD_GUID} initialised.");
-        }
+            if (_initialised)
+                return;
 
-        private static void Initialise()
-        {
-            _bundle = LoadAssetBundle();
+            LoadAssetBundle();
             _spookboxItem = _bundle.LoadAsset<Item>("Spookbox");
             _spookboxItem.itemObject.AddComponent<SpookboxBehaviour>();
 
@@ -79,20 +71,59 @@ namespace Spookbox
             _spookboxItem.SetDefaultTooltips($"{ShopLocalisation.UseGlyph} Play;{ShopLocalisation.Use2Glyph} Next Track");
 
             Mixtape.LoadTracks();
+
+            _initialised = true;
+            Debug.Log($"{MOD_GUID} setup complete.");
         }
 
-#if STEAM
-        private static IEnumerator Deferred_LoadSettings()
+        internal static void InitialiseSettings()
         {
-            yield return new WaitUntil(() => GameHandler.Instance.SettingsHandler != null);
-            InitialiseSettings();
-        }
-#endif
+            if (_initialisedSettings)
+                return;
 
-        private static AssetBundle LoadAssetBundle()
+            cb_onLobbyCreated = Callback<LobbyCreated_t>.Create(Steam_LobbyCreated);
+
+            InitialiseGameSetting<BoomboxAlertSetting>();
+            InitialiseGameSetting<BoomboxInfiniteBatterySetting>();
+            InitialiseGameSetting<BoomboxPriceSetting>();
+
+            _initialisedSettings = true;
+
+            Debug.Log($"{MOD_GUID} settings initialised.");
+        }
+
+        private static void Steam_LobbyCreated(LobbyCreated_t e)
         {
-            var path = Path.Join(PluginDirPath, "spookbox");
-            return AssetBundle.LoadFromFile(path);
+            if (e.m_eResult != EResult.k_EResultOK)
+            {
+                return;
+            }
+            ApplyGameSetting<BoomboxAlertSetting>();
+            ApplyGameSetting<BoomboxInfiniteBatterySetting>();
+            ApplyGameSetting<BoomboxPriceSetting>();
+        }
+
+        private static void InitialiseGameSetting<T>() where T : Setting, new()
+        {
+            if (GameHandler.Instance.SettingsHandler.GetSetting<T>() == null)
+            {
+                GameHandler.Instance.SettingsHandler.AddSetting(new T());
+            }
+        }
+
+        private static void ApplyGameSetting<T>() where T : Setting, new()
+        {
+            var setting = GameHandler.Instance.SettingsHandler.GetSetting<T>();
+            if (setting != null)
+            {
+                setting.ApplyValue();
+            }
+        }
+
+        private static void LoadAssetBundle()
+        {
+            var path = Path.Join(PluginDirPath, ASSETBUNDLE_NAME);
+            _bundle = AssetBundle.LoadFromFile(path);
         }
     }
 }
