1ec9539640b6a4dc07425aa2f1e4e6413117a032
Bars Margetsch <45538919+Xerren09@users.noreply.github.com>
2025-04-05 21:38:26 +0300

    Add missing settings to manual load, refactoring

--- SpookboxPlugin.cs
+++ SpookboxPlugin.cs
@@ -7,33 +7,15 @@ using Spookbox.Settings;
 using Steamworks;
 using Zorro.Settings;
 
-/*
-    TASKS:
-
-    DONE: Add infinite battery setting
-    DONE: Price setting setup
-    DONE: Keep the beats flowing even if the boombox is stashed
-    DONE: Respect local volume override when detached
-    DONE: Scroll to swap tracks
-    
-    TODO: Right click hold on shoulder - plays louder?
-    TODO: Localise strings
-*/
-
 namespace Spookbox
 {
-    // This partial class file is used purely to separate the mess that is the multi target setup.
-    // Define mod setup changes between the vanilla mod loader and bepinx
-    public partial class SpookboxPlugin
+    public class SpookboxPlugin
     {
         public const string MOD_GUID = "xerren.cwspookbox";
         public const string MOD_NAME = "Spöökbox";
         public const string MOD_VER = ThisAssembly.AssemblyVersion;
         private const string ASSETBUNDLE_NAME = "spookbox";
         internal static string PluginDirPath => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-
-        private static bool _initialised = false;
-        private static bool _initialisedSettings = false;
 
         /// <summary>
         /// Asset bundle containing all the mod's assets.
@@ -61,10 +43,12 @@ namespace Spookbox
         /// </summary>
         internal static void InitialisePlugin()
         {
-            if (_initialised)
+            if (cb_onLobbyCreated != null)
                 return;
 
-            LoadAssetBundle();
+            var path = Path.Join(PluginDirPath, ASSETBUNDLE_NAME);
+            _bundle = AssetBundle.LoadFromFile(path);
+
             _spookboxItem = _bundle.LoadAsset<Item>("Spookbox");
             _spookboxItem.itemObject.AddComponent<SpookboxBehaviour>();
 
@@ -75,7 +59,9 @@ namespace Spookbox
 
             Mixtape.LoadTracks();
 
-            _initialised = true;
+            // Detect local lobby creation so we can overwrite any potential leftover settings
+            cb_onLobbyCreated = Callback<LobbyCreated_t>.Create(Steam_LobbyCreated);
+
             Debug.Log($"{MOD_GUID} setup complete.");
         }
 
@@ -86,24 +72,20 @@ namespace Spookbox
         /// This is mainly important for the scenario where the player installed the mod after the game was launched,
         /// as settings are not loaded into the global settings repository in that workflow.
         /// </remarks>
-        internal static void InitialiseSettings()
+        internal static void RegisterSettings()
         {
-            if (_initialisedSettings)
-                return;
-
-            // Detect local lobby creation so we can overwrite any potential leftover settings
-            cb_onLobbyCreated = Callback<LobbyCreated_t>.Create(Steam_LobbyCreated);
-
-            InitialiseGameSetting<BoomboxAlertSetting>();
-            InitialiseGameSetting<BoomboxInfiniteBatterySetting>();
-            InitialiseGameSetting<BoomboxPriceSetting>();
+            // Key bindings
+            RegisterGameSetting<BoomboxLocalVolumeSetting>();
+            RegisterGameSetting<BoomboxVolumeUpBindSetting>();
+            RegisterGameSetting<BoomboxVolumeDownBindSetting>();
+            // Mechanic settings
+            RegisterGameSetting<BoomboxAlertSetting>();
+            RegisterGameSetting<BoomboxInfiniteBatterySetting>();
+            RegisterGameSetting<BoomboxPriceSetting>();
             // "Fake" settings
-            InitialiseGameSetting<BoomboxOpenTracksFolderSetting>();
-            InitialiseGameSetting<BoomboxRescanMixtapeFolderSetting>();
-
-            _initialisedSettings = true;
-
-            Debug.Log($"{MOD_GUID} settings initialised.");
+            RegisterGameSetting<BoomboxOpenTracksFolderSetting>();
+            RegisterGameSetting<BoomboxRescanMixtapeFolderSetting>();
+            Debug.Log($"{MOD_GUID} settings manually registered.");
         }
 
         private static void Steam_LobbyCreated(LobbyCreated_t e)
@@ -121,7 +103,7 @@ namespace Spookbox
         /// Add a setting to <see cref="SettingsHandler"/> if an instance has not been added already.
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        private static void InitialiseGameSetting<T>() where T : Setting, new()
+        private static void RegisterGameSetting<T>() where T : Setting, new()
         {
             if (GameHandler.Instance.SettingsHandler.GetSetting<T>() == null)
             {
@@ -140,12 +122,6 @@ namespace Spookbox
             {
                 setting.ApplyValue();
             }
-        }
-
-        private static void LoadAssetBundle()
-        {
-            var path = Path.Join(PluginDirPath, ASSETBUNDLE_NAME);
-            _bundle = AssetBundle.LoadFromFile(path);
         }
     }
 }
