6b3673d0ac5fcdd3a79a2e04db58b2c62b11d893
Bars Margetsch <45538919+Xerren09@users.noreply.github.com>
2025-11-20 16:51:48 +0200

    Add new error case handlers
    Also clarifies the error popups

--- PluginLoader.STEAM.cs
+++ PluginLoader.STEAM.cs
@@ -1,5 +1,6 @@
 ﻿#if STEAM
 using ContentWarningShop;
+using Steamworks;
 using System.Collections;
 using UnityEngine;
 
@@ -12,20 +13,23 @@ namespace Spookbox
             Initialising this mod is MUCH more difficult on Steam because of the external dependency, ShopAPI. Realistically
             I could just let it rip, but I want to make sure if things go very wrong the player isn't left confused.
 
-            When a Workshop Item is subscribed to WHILE the game is running, the mod loader works quite differently:
-            1. Settings must be manually registered.
-            2. Dependency order is not enforced.
-                This one SUCKS. Normally Steam correctly resolves that ShopAPI must be loaded before Spookbox, but when installing
-                the mod WHILE the game is running, it's either a cointoss or reverse order (couldn't figure it out) which one gets downloaded
-                first and loaded. 
-                In some cases Spookbox is loaded first, which throws a TypeLoadException since things like SynchronisedMetadata
-                are pulled from ShopAPI. 
-         */
+            During standard install, Spookbox and ShopAPI are downloaded and their load orders are set by Steam automatically.
+                After game start settings are pulled and registered automatically by the game.
 
-        private static Version _minDepVersion = new Version(ShopApiPlugin.MOD_VER);
+            When a Workshop Item is subscribed to *while* the game is running, the game's mod loader works quite differently:
+            1. Settings must be manually registered.
+            2. Mod dependency order is not enforced: items are loaded in the other they have been subscribed to. This means ShopAPI
+               loads second, and Spookbox faults because things like SynchronisedMetadata are missing.
+         */
+        /// <summary>
+        /// ShopAPI workshop handle ID
+        /// </summary>
+        const ulong w_id = 3408837293;
+
+        private static System.Version _minDepVersion = new System.Version(ShopApiPlugin.MOD_VER);
         static PluginLoader() 
         {
-            Debug.Log($"{SpookboxPlugin.MOD_GUID} loading via vanilla mod loader.");
+            Logger.Log($"Loading via vanilla mod loader.");
             Coroutine routine = null;
             try
             {
@@ -33,34 +37,45 @@ namespace Spookbox
                 Action proxyCall = () => { SpookboxPlugin.InitialisePlugin(); };
                 proxyCall();
                 routine = GameHandler.Instance.StartCoroutine(Deferred_LoadSettings());
-                Debug.Log($"{SpookboxPlugin.MOD_GUID} loaded.");
+                Logger.Log($"Finished loading.");
             }
             catch (Exception ex)
             {
+                // If installed after the game is started, SettingsHandler is already initialised
                 var isRuntimeInstallFault = GameHandler.Instance.SettingsHandler != null;
-                Debug.LogError($"{SpookboxPlugin.MOD_GUID} IsRuntimeInstallFault: {isRuntimeInstallFault}");
+                Logger.LogError($"IsRuntimeInstallFault: {isRuntimeInstallFault}");
                 if (routine != null)
                 {
                     GameHandler.Instance.StopCoroutine(routine);
                 }
+
                 if (isRuntimeInstallFault && IsDependencyLoaded() == false)
                 {
-                    Debug.LogError($"{SpookboxPlugin.MOD_GUID} failed to load: {ShopApiPlugin.MOD_GUID} not loaded during late install.");
+                    Logger.LogError($"Failed to load: {ShopApiPlugin.MOD_GUID} not loaded during late install.");
                     ShowDependencyLateInstallRestartPrompt();
                 }
                 else if (IsDependencyLoaded() == false)
                 {
-                    Debug.LogError($"{SpookboxPlugin.MOD_GUID} failed to load: {ShopApiPlugin.MOD_GUID} not loaded.");
-                    ShowMissingDependencyErrorPrompt();
+                    if (IsDependencyDownloaded() == false)
+                    {
+                        Logger.LogError($"Failed to load: {ShopApiPlugin.MOD_GUID} missing.");
+                        ShowMissingDependencyErrorPrompt();
+                    }
+                    if (IsDependencyEnabled() == false)
+                    {
+                        Logger.LogError($"Failed to load: {ShopApiPlugin.MOD_GUID} present but not enabled.");
+                        ShowDisabledDependencyErrorPrompt();
+                    }
                 }
                 else if (IsDependencyVersionCorrect() == false)
                 {
-                    Debug.LogError($"{SpookboxPlugin.MOD_GUID} failed to load: {ShopApiPlugin.MOD_GUID} version incorrect.");
+                    Logger.LogError($"Failed to load: {ShopApiPlugin.MOD_GUID} version incorrect.");
                     ShowDependencyVersionErrorPrompt();
                 }
                 else
                 {
-                    Debug.LogError($"{SpookboxPlugin.MOD_GUID} failed to load ({ex.GetType().Name}).");
+                    Logger.LogError($"Failed to load ({ex.GetType().Name}).");
+                    ShowGenericErrorPrompt();
                 }
                 Debug.LogException(ex);
             }
@@ -94,13 +109,33 @@ namespace Spookbox
             return target != null;
         }
 
+        private static bool IsDependencyDownloaded()
+        {
+#if RELEASE
+            EItemState dependencyState = (EItemState)SteamUGC.GetItemState(new PublishedFileId_t(w_id));
+            return dependencyState.HasFlag(EItemState.k_EItemStateInstalled) || dependencyState.HasFlag(EItemState.k_EItemStateSubscribed);
+#elif DEBUG
+            return true;
+#endif
+        }
+
+        private static bool IsDependencyEnabled()
+        {
+#if RELEASE
+            EItemState dependencyState = (EItemState)SteamUGC.GetItemState(new PublishedFileId_t(w_id));
+            return !dependencyState.HasFlag(EItemState.k_EItemStateDisabledLocally);
+#elif DEBUG
+            return true;
+#endif
+        }
+
         /// <summary>
         /// Error message shown if the mod is installed from the Steam Workshop while the game is running
         /// </summary>
         private static void ShowDependencyLateInstallRestartPrompt()
         {
             var options = new ModalOption[2] { new ModalOption("Close Game", () => { Application.Quit(); }), new ModalOption("Ignore") };
-            Modal.Show($"{SpookboxPlugin.MOD_NAME} Dependency Not Loaded", "Spöökbox depends on the ShopAPI mod, which is not loaded. This can often be caused by installing the mod while the game is running. Please ensure you've subscribed to the ShopAPI mod, and restart your game in order for the mod to work. If you run into further issues please read the Workshop page, and don't hesitate to reach out.", options);
+            Modal.Show($"{SpookboxPlugin.MOD_NAME} Late Install", "Spöökbox depends on the ShopAPI mod, which needs to be loaded first. This can often be caused by installing the mod while the game is running. Please ensure you've subscribed to the ShopAPI mod and restart your game, otherwise Spöökbox will not work for the current session.", options);
         }
 
         /// <summary>
@@ -108,8 +143,17 @@ namespace Spookbox
         /// </summary>
         private static void ShowMissingDependencyErrorPrompt()
         {
-            var options = new ModalOption[2] { new ModalOption("Close Game", () => { Application.Quit(); }), new ModalOption("Ignore") };
+            var options = new ModalOption[2] { new ModalOption("Close Game", () => { Application.Quit(); }), new ModalOption("Ignore")};
             Modal.Show($"{SpookboxPlugin.MOD_NAME} Dependency Not Installed", "Spöökbox depends on the ShopAPI mod, which is not installed. The game will NOT work with Spöökbox enabled while ShopAPI is missing. Please ensure you've subscribed to the ShopAPI mod and restart your game, or disable Spöökbox.", options);
+        }
+
+        /// <summary>
+        /// Error message shown if ShopAPI is disabled in steam
+        /// </summary>
+        private static void ShowDisabledDependencyErrorPrompt()
+        {
+            var options = new ModalOption[2] { new ModalOption("Close Game", () => { Application.Quit(); }), new ModalOption("Ignore") };
+            Modal.Show($"{SpookboxPlugin.MOD_NAME} Dependency Disabled", "Spöökbox depends on the ShopAPI mod, which is not enabled. The game will NOT work with Spöökbox enabled while ShopAPI is disabled. To enable ShopAPI, go to your steam library, right click Content Warning, go to Properties -> Workshop, and enable the ShopAPI item, then restart the game.", options);
         }
 
         /// <summary>
@@ -120,7 +164,7 @@ namespace Spookbox
             var options = new ModalOption[2] { new ModalOption("Close Game", () => { Application.Quit(); }), new ModalOption("Ignore") };
             var assemblies = AppDomain.CurrentDomain.GetAssemblies();
             var target = Array.Find(assemblies, a => a.GetName().Name == "ShopAPI");
-            var presentVer = new Version();
+            var presentVer = new System.Version();
             if (target != null)
             {
                 presentVer = target.GetVersion();
